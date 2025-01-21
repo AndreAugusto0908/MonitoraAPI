ceea767b1e5bd4783be896b04d0b4fbec5934220
Andre Augusto Silva Carvalho <andreaugustosilvacarvalho@gmail.com>
2025-01-21 14:00:30 -0300

    Docker

--- MonitoraAPI/obj/Debug/net9.0/MonitoraAPI.AssemblyInfo.cs
+++ MonitoraAPI/obj/Debug/net9.0/MonitoraAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonitoraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1807db255d5d8cefef6df989359d4bcd3d6617fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonitoraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonitoraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
